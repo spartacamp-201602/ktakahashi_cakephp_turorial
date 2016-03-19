7b262f08399150b037beb4436cb60f80b368a73d
K.Takahashi <nike.taka@gmail.com>
2016-03-19 13:48:50 +0900

    新規投稿作成

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -4,7 +4,10 @@
 // アクション ... XXXXControllerの中に定義されているメソッドのこと
 class PostsController extends AppController {
 
+    //使用するヘルパー
     public $helpers = array('Html', 'Form');
+
+    public $components = array('Flash');
 
     public function index() {
 
@@ -22,5 +25,29 @@ class PostsController extends AppController {
     public function show($id) {
         $post = $this->Post->findById($id);
         $this->set('post', $post);
+    }
+
+    public function add()
+    {
+        if ($this->request->is('post'))
+        {
+            // 保存処理
+            if ($this->Post->save($this->request->data))
+            {
+                // 保存に成功した場合
+
+                // フラッシュメッセージ
+                $this->Flash->success('新しい記事を追加しました');
+
+                // リダイレクト
+                return $this->redirect(array('action' => 'index'));
+
+            } else
+            {
+                // 保存に失敗した場合
+                $this->Flash->error('保存できませんでした。。。');
+            }
+
+        }
     }
 }
